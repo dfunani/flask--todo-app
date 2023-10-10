757b0a0154e428ec3ac58b104eb5c697975e745f
dfunani <dfunani@live.co.za>
2023-10-10 23:32:49 +0200

    added db err

--- application/tests/models/test_sample.py
+++ application/tests/models/test_sample.py
@@ -1,4 +1,3 @@
-from app import app
 from models.todo import Users
 
 # Simple integration testing (DB Models exists)
@@ -13,12 +12,3 @@ def test_users():
     assert user.email == "dfunani@gmail.com"
     assert user.username == "delali"
     assert user.hashcode == "db.Column(db.String(255), nullable=False))"
-
-# Simple Unit testing login Endpoint
-def test_login():
-    # Simple test of the token endpoint
-    with app.test_client() as testClient:
-        response = testClient.get('/login')
-        assert response.status_code == 200
-        # Checks the login page is returned
-        assert b"Flask TODO App" in response.data
